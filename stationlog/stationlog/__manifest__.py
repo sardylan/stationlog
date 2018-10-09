dae87bde997516b29a0b376693a9100cbad1d3dd
sardylan <sardylan@gmail.com>
2018-10-09 20:45:50 +0200

    Adds main module files

---
+++ stationlog/stationlog/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Station Log",
+    "version": "12.0.0.0.0",
+    "category": "Extra Tools",
+    "summary": "HAM Station log",
+    "depends": [
+    ],
+    "data": [
+    ],
+    "application": True
+}
