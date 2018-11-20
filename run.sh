f85e97d904f31beede21bae2eda6f942c04b4fca
sardylan <sardylan@gmail.com>
2018-11-20 22:19:18 +0100

    Adds simple and fust run script

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+STATIONLOG_HOME="$(pwd)"
+VENV_HOME="${STATIONLOG_HOME}/venv"
+
+PATH="${VENV_HOME}/bin:${PATH}"
+
+python3 \
+    ./odoo/odoo-bin \
+    --addons-path=odoo/odoo/addons,odoo/addons,hamutility,stationlog,repeaters \
+    --db_host=127.0.0.1 \
+    --db_port=5432 \
+    --db_user=stationlog \
+    --db_password=stationlog \
+    --database=stationlog \
+    --db-filter=stationlog \
+    --http-port=8069
