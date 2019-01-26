a7a022ce63196ba610667a6d97d68bd96ded624f
sardylan <sardylan@gmail.com>
2019-01-26 23:08:07 +0100

    Adds contest required fields and QSOs count

--- stationlog/stationlog/models/contest.py
+++ stationlog/stationlog/models/contest.py
@@ -10,19 +10,40 @@ class Contest(models.Model):
     name = fields.Char(
         string="Name",
         help="Contest name",
-        track_visibility="onchange"
+        track_visibility="onchange",
+        required=True
     )
 
     ts_start = fields.Datetime(
         string="Start",
-        track_visibility="onchange"
+        track_visibility="onchange",
+        required=True
     )
 
     ts_end = fields.Datetime(
         string="End",
-        track_visibility="onchange"
+        track_visibility="onchange",
+        required=True
+    )
+
+    qso_ids = fields.One2many(
+        string="QSOs",
+        help="QSOs related to this contest",
+        comodel_name="stationlog.qso",
+        inverse_name="contest_id"
     )
 
     note = fields.Html(
         string="Note"
     )
+
+    qso_count = fields.Integer(
+        string="QSOs number",
+        help="Number of QSOs",
+        readonly=True,
+        compute="_compute_qso_count"
+    )
+
+    def _compute_qso_count(self):
+        for rec in self:
+            rec.qso_count = len(rec.qso_ids)
