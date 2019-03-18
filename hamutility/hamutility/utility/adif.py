b53639330b89308b57c1855f3808d71c10b2dd5d
sardylan <sardylan@gmail.com>
2019-03-18 01:10:50 +0100

    Fixes for stripped strings

--- hamutility/hamutility/utility/adif.py
+++ hamutility/hamutility/utility/adif.py
@@ -28,7 +28,7 @@ class AdifUtility(models.AbstractModel):
 
         for item in items:
             tag_param = item[0].upper()
-            tag_value = item[1].strip()
+            tag_value = item[1]
 
             item_split = tag_param.split(":")
 
@@ -39,31 +39,33 @@ class AdifUtility(models.AbstractModel):
             if len(item_split) > 1:
                 item_length = int(item_split[1].strip())
 
-            if len(tag_value) != item_length:
-                print("Invalid values: %s" % item)
-
             if len(item_split) > 2:
                 item_type = item_split[2].strip().upper()
 
-            item_value = tag_value
+            item_value = tag_value.strip()
+
+            if item_length:
+                item_value = tag_value[:item_length].strip()
 
             if item_param in ["TIME_ON", "TIME_OFF"]:
-                item_value = time(hour=int(tag_value[0:2]), minute=int(tag_value[2:4]), second=int(tag_value[4:6]))
+                second = len(item_value) > 4 and int(item_value[4:6]) or 0
+                item_value = time(hour=int(item_value[0:2]), minute=int(item_value[2:4]), second=second)
             elif item_param in ["QSO_DATE", "QSLSDATE"]:
-                item_value = date(year=int(tag_value[0:4]), month=int(tag_value[4:6]), day=int(tag_value[6:8]))
+                item_value = date(year=int(item_value[0:4]), month=int(item_value[4:6]), day=int(item_value[6:8]))
             elif item_param in ["FREQ", "FREQ_RX"]:
-                item_value = int(float(tag_value) * 1000000)
+                item_value = int(float(item_value) * 1000000)
             elif item_param in ["GRIDSQUARE"]:
-                item_value = "%s%s" % (tag_value[0:4].upper(), tag_value[4:8].lower())
+                item_value = "%s%s" % (item_value[0:4].upper(), item_value[4:8].lower())
 
             elif item_type == "D":
-                item_value = date(year=int(tag_value[0:4]), month=int(tag_value[4:6]), day=int(tag_value[6:8]))
+                item_value = date(year=int(item_value[0:4]), month=int(item_value[4:6]), day=int(item_value[6:8]))
             elif item_type == "T":
-                item_value = time(hour=int(tag_value[0:2]), minute=int(tag_value[2:4]), second=int(tag_value[4:6]))
+                second = len(item_value) > 4 and int(item_value[4:6]) or 0
+                item_value = time(hour=int(item_value[0:2]), minute=int(item_value[2:4]), second=second)
             elif item_type == "B":
-                item_value = bool(tag_value.upper() == "Y")
+                item_value = bool(item_value.upper() == "Y")
             elif item_type == "N":
-                item_value = int(tag_value)
+                item_value = int(item_value)
 
             if mode == MODE_HEADER:
                 if item_param == "EOH":
