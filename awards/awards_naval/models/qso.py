23570d47134548634e5c6bea06aa9570c76f552e
sardylan <sardylan@gmail.com>
2019-04-03 13:06:06 +0200

    Change DUPE as selection

--- awards/awards_naval/models/qso.py
+++ awards/awards_naval/models/qso.py
@@ -21,6 +21,11 @@ SELECTION_MODE = [
     ("DIGI", "DIGI")
 ]
 
+SELECTION_DUPE = [
+    ("normal", "Normal"),
+    ("dupe", "DUPE")
+]
+
 _logger = logging.getLogger(__name__)
 
 
@@ -69,6 +74,14 @@ class QSO(models.Model):
         readonly=True
     )
 
+    dupe = fields.Selection(
+        string="DUPE",
+        selection=SELECTION_DUPE,
+        readonly=True,
+        required=True,
+        default="normal"
+    )
+
     rst_tx = fields.Char(
         string="RST TX",
         compute="compute_rst",
@@ -81,32 +94,48 @@ class QSO(models.Model):
         readonly=True
     )
 
-    dupe = fields.Boolean(
-        string="DUPE",
+    country_id = fields.Many2one(
+        string="Country",
+        comodel_name="hamutility.country",
+        compute="compute_country",
         readonly=True,
+        store=True
     )
 
+    ts_date = fields.Date(
+        string="Date",
+        compute="compute_ts_date",
+        readonly=True,
+        store=True
+    )
+
+    ts_time = fields.Char(
+        string="Time",
+        compute="compute_ts_date_time",
+        readonly=True,
+        store=True
+    )
+
+    @api.depends("mode")
     def compute_rst(self):
         for rec in self:
             rst = rec.mode in ["CW", "DIGI"] and "599" or "59"
             rec.rst_tx = rst
             rec.rst_rx = rst
 
-    country_id = fields.Many2one(
-        string="Country",
-        comodel_name="hamutility.country",
-        compute="_compute_country",
-        readonly=True,
-        store=True
-    )
-
     @api.depends("callsign")
-    def _compute_country(self):
+    def compute_country(self):
         utility_callsign_onj = self.env["hamutility.utility_callsign"]
 
         for rec in self:
             if rec and rec.callsign:
                 rec.country_id = utility_callsign_onj.get_country(rec.callsign)
+
+    @api.depends("ts")
+    def compute_ts_date_time(self):
+        for rec in self:
+            rec.ts_date = rec.ts.strftime("%Y-%m-%d")
+            rec.ts_time = rec.ts.strftime("%H%M%S")
 
     @api.model
     def action_update_reference_auto(self):
@@ -167,7 +196,7 @@ class QSO(models.Model):
     @api.model
     def action_compute_dupe(self):
         qso_ids = self.search([])
-        qso_ids.write({"dupe": False})
+        qso_ids.write({"dupe": "normal"})
 
         qso_first = self.search([], limit=1, order="ts ASC")
         qso_last = self.search([], limit=1, order="ts DESC")
@@ -178,70 +207,34 @@ class QSO(models.Model):
         datetime_interval = datetime_last - datetime_first
         days = datetime_interval.days + 1
 
-        callsign_list = [x["callsign"] for x in self.read_group(domain=[], fields=["callsign"], groupby=["callsign"]) if
-                         x["callsign"]]
+        qso_ids_group = self.read_group(domain=[], fields=["callsign"], groupby=["callsign"])
+        callsign_list = [x["callsign"] for x in qso_ids_group if x["callsign"]]
 
         for callsign in callsign_list:
             for i in range(0, days):
                 datetime_day = datetime_first + datetime.timedelta(days=i)
 
-                ts_start = datetime_day.strftime("%Y-%m-%d 00:00:00")
-                ts_end = datetime_day.strftime("%Y-%m-%d 23:59:59")
+                self.check_dupe(callsign, datetime_day, "CW")
+                self.check_dupe(callsign, datetime_day, "SSB")
+                self.check_dupe(callsign, datetime_day, "DIGI")
 
-                self.check_dupe_cw(callsign, datetime_day, ts_end, ts_start)
-                self.check_dupe_ssb(callsign, datetime_day, ts_end, ts_start)
-                self.check_dupe_digi(callsign, datetime_day, ts_end, ts_start)
+    def check_dupe(self, callsign, datetime_day, mode):
+        ts_start = datetime_day.strftime("%Y-%m-%d 00:00:00")
+        ts_end = datetime_day.strftime("%Y-%m-%d 23:59:59")
 
-    def check_dupe_cw(self, callsign, datetime_day, ts_end, ts_start):
         qso_ids = self.search([
             ("callsign", "=", callsign),
-            ("mode", "=", "CW"),
+            ("mode", "=", mode),
             ("ts", ">=", ts_start),
             ("ts", "<=", ts_end),
         ], order="ts ASC")
 
         if len(qso_ids) > 1:
-            _logger.info("DUPE for %s on %s in CW with %d QSO" % (
-                callsign, datetime_day.strftime("%Y-%m-%d"), len(qso_ids)
+            _logger.info("DUPE for %s on %s in %s with %d QSO" % (
+                callsign, datetime_day.strftime("%Y-%m-%d"), mode, len(qso_ids)
             ))
 
             count = 0
             for qso_id in qso_ids:
-                qso_id.dupe = bool(count > 0)
-                count += 1
-
-    def check_dupe_ssb(self, callsign, datetime_day, ts_end, ts_start):
-        qso_ids = self.search([
-            ("callsign", "=", callsign),
-            ("mode", "=", "SSB"),
-            ("ts", ">=", ts_start),
-            ("ts", "<=", ts_end),
-        ], order="ts ASC")
-
-        if len(qso_ids) > 1:
-            _logger.info("DUPE for %s on %s in SSB with %d QSO" % (
-                callsign, datetime_day.strftime("%Y-%m-%d"), len(qso_ids)
-            ))
-
-            count = 0
-            for qso_id in qso_ids:
-                qso_id.dupe = bool(count > 0)
-                count += 1
-
-    def check_dupe_digi(self, callsign, datetime_day, ts_end, ts_start):
-        qso_ids = self.search([
-            ("callsign", "=", callsign),
-            ("mode", "=", "DIGI"),
-            ("ts", ">=", ts_start),
-            ("ts", "<=", ts_end),
-        ], order="ts ASC")
-
-        if len(qso_ids) > 1:
-            _logger.info("DUPE for %s on %s in DIGI with %d QSO" % (
-                callsign, datetime_day.strftime("%Y-%m-%d"), len(qso_ids)
-            ))
-
-            count = 0
-            for qso_id in qso_ids:
-                qso_id.dupe = bool(count > 0)
+                qso_id.dupe = "dupe" if count > 0 else "normal"
                 count += 1
